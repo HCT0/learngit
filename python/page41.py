40358080bbcaa0aad421e83a4990037caa5a9a66
hct <2198025618@qq.com>
2019-09-05 20:59:15 +0800

    这个是爬取酷狗的代码

---
+++ python/page41.py
@@ -0,0 +1,27 @@
+import Requests
+from bs4 import BeautifulSoup
+import time
+
+headers = {
+    'user-agent' :' Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
+}
+def get_info(url):
+    wb_data = Requests.get(url,headers = headers)
+    soup = BeautifulSoup(wb_data.text,'lxml')
+    ranks = soup.select('span.pc_temp_num')
+    titles = soup.select('div.pc_temp_songlist> ul > li> a ')
+    times = soup.select('span.pc_temp_tips_r > span ')
+    for rank in ranks:
+        data ={
+            'rank':rank.get_text().strip(),
+            'singer':title.get_text().split('-')[0],
+            'song':title.get_text().split('-')[1],
+            'time':time.get_text().strip()
+        }
+        print(data)
+    
+if __name__ == '__main__':
+    urls = ['https://www.kugou.com/yy/rank/home/{}-8888.html'.format(str(i)) for i in rang(1,24)]
+    for url in urls:
+        get_info(url)
+        time.sleep(2)
