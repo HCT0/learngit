db555ef714754648cef608abff161d90d69e54fa
hct <2198025618@qq.com>
2019-09-06 17:33:00 +0800

    进行修改

--- python/page53.py
+++ python/page53.py
@@ -4,25 +4,25 @@ import re
 import time
 
 headers ={
-    'User-Agent':' Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
+    'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
 }
 #打开指定的文件
-f =open('C:/Users/龙重文/python/doupo.txt','a+')
+f =open('C:/Users/龙重文/learngit/python/doupo.txt','a+')
 #一个实现爬取内容的类
 def get_info(url):
     res = requests.get(url,headers = headers)
-    if res.status_cpde = 200:
+    if res.status_cpde == 200:
         #. 爬取任意一个字符，*和？爬取0个或者1或者无限个
         #findall是以列表的形式来返回获取的数据
-        contens = re.findall('<p>(.*?)</p>,res.content.decode('utf-8'),re.s)
+        contents = re.findall('<p>(.*?)</p>',res.content.decode('utf-8'),re.s)
         for content in contents:
             #写入文件
-            f.write(content:'\n')
+            f.write(content+'\n') 
     else:
         pass
 if __name__ == '__main__':
     urls =['http://www.doupoxs.com.doupocangqiong/{}.html'.format(str(i)) for i in range(2,1665)]
     for url in urls:
-        get_info(url):
+        get_info(url)
         time.sleep(2)
 f.close()
--- python/page56.py
+++ python/page56.py
@@ -7,24 +7,36 @@ headers = {
 info_lists = []
 
 def judement_sex(class_name):
-    if class_name =='womenIcon'
+    if class_name =='womenIcon':
         return '女'
-    else：
+    else:
         return '男'
 
 def get_info(url):
     res = requests.get(url,headers = headers)
-    ids = re.findal('<h2>(.*?)</h2>',res.text,re.S
+    ids = re.findall('<h2>(.*?)</h2>',res.text,re.S)
     for id in zip (ids):
         info = {
-            'id':id
+            'id':id,
         }
     info_lists.append(info)
 
 if __name__ =='__main__':
-    urls =['http://www.qiushibaike.com/text/page/{}/'.format(str{i}) for i in range(1,36)]
-    for url in urls 
+    #format
+    #循环
+    #列表
+    urls =['http://www.qiushibaike.com/text/page/{}/'.format(str(i)) for i in range(1,36)]
+    for url in urls :
         get_info(url)
-    for info_list in info_list:
-        f =open('C:/Users/龙重文/python/qiushibaike.txt','a+')
+    for info_list  in info_lists:
+        f =open('C:/Users/龙重文/learngit/python/qiushibaike.txt','a+')
+        f.write(info_list['id']+'\n')
+        f.close()
+
+    """
+        浏览器中select和xpath的使用
+        git在VScode的使用
+        
+    """
     
+                                                                                                                                                                                                                                                                                                                                                                                                                  
