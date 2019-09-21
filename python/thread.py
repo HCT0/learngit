fbc3cc4226198f5a60b3794a299373012445d469
HCT--git <2198025618@qq.com>
2019-09-21 11:05:26 +0800

    python 实现倒计时

--- python/thread.py
+++ python/thread.py
@@ -1,36 +1,34 @@
 '''
     使用多线程实现倒计时效果
 '''
-import time
 import datetime
-from multiproessing import Process
+from multiprocessing import Process
 
 def calculate_time(year,month,day):
     date1 = datetime.datetime(year,month,day)
-    a = datetime.date.today()
     
-    date2 = datetime.datetime(a.yearr,a.month,a.day)
-    all=(data1-data1).days
+    a = datetime.date.today()
+    date2 = datetime.datetime(a.year,a.month,a.day)
+    all=(date1-date2).days
     return all
 
 def print_time():
     print("今天的日期:")
     a = datetime.date.today()
     print(a.year,"年",a.month,"月",a.day,"日")
-    print("\n")
-    print("距离：",'\n')
+    print("距离：")
 
 def gaokao():
     gaokao = calculate_time(2020,6,6)
-    print("距离2020高考还有：" ,gaokao,"天")
+    print("2020高考还有：" ,gaokao,"天")
 
 def dongjing():
-    dongjin = calculate_time(2020,7,24)
-    print("距离东京奥运会还有：",dongjing,"天")
+    dongjing = calculate_time(2020,7,24)
+    print("东京奥运会还有：",dongjing,"天")
 
 def beijin():
     beijin= calculate_time(2022,2,4)
-    print("距离北京冬奥会还有：",beijin,"天")
+    print("北京冬奥会还有：",beijin,"天")
 
 def kata():
     kata= calculate_time(2022,11,21)
@@ -38,21 +36,17 @@ def kata():
 
 
 if __name__ == '__main__':
-    print_time = Process(target = print_time)
-    print_time.start()
-    gaokao = Process(target = gaokao)
-    gaokao.start()
-    dongjing = Process(target = dongjing)
-    dongjing.start()
+    print_time_o = Process(target = print_time)
+    print_time_o.start()
+    gaokao_o = Process(target = gaokao)
+    gaokao_o.start()
+    dongjing_o = Process(target = dongjing)
+    dongjing_o.start()
     
-    beijin = Process(target = beijin)
-    beijin.start()
+    beijin_o = Process(target = beijin)
+    beijin_o.start()
     
-    kata = Process(target = kata)
-    kata.start()
+    kata_o = Process(target = kata)
+    kata_o.start()
 
-    print_time.join()
-    gaokao.join()
-    dongjing.join()
-    beijin.join()
-    kata.join()
+  
